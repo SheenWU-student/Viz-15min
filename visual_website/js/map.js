3a460aa3bfc642f0de5f7fec1425e7815fd12412
SheenWU-student <ucfnxwu@ucl.ac.uk>
2024-05-06 14:09:33 +0100

    change legend

--- visual_website/js/map.js
+++ visual_website/js/map.js
@@ -13,12 +13,19 @@ map.on('load', function() {
         data: 'data/ACL_trans.geojson'
     });
 
+    map.addSource('ACL', {
+        type: 'geojson',
+        data: 'data/london-boroughs_1179.geojson'
+    });
+
     map.getSource('ACL').on('data', function(e) {
         if (e.isSourceLoaded) {
             // 当数据完全加载时打印出来
             console.log('GeoJSON data:', e.source.data);
         }
     });
+
+    map.get
 
     map.addLayer({
         id: 'geojsonLayer',
@@ -31,12 +38,16 @@ map.on('load', function() {
                 'interpolate',
                 ['linear'],
                 ['get', 'employment_15'],  // 这里使用数据中的employment_15字段
-                0, '#F2F12D',              // employment_15为0时的颜色
-                10000, '#EED322',          // employment_15为10000时的颜色
-                50000, '#E6B71E',          // employment_15为50000时的颜色
-                100000, '#DA9C20',         // employment_15为100000时的颜色
-                200000, '#CA8323',         // employment_15为200000时的颜色
-                500000, '#B86B25'          // employment_15为500000时的颜色
+                0, '#053061',              // employment_15为0时的颜色
+                1000, '#2166ac',          // employment_15为10000时的颜色
+                5000, '#4393c3',          // employment_15为50000时的颜色
+                10000, '#92c5de',         // employment_15为100000时的颜色
+                20000, '#fddbc7',         // employment_15为200000时的颜色
+                30000, '#f4a582',          // employment_15为300000时的颜色
+                40000, '#d6604d',
+                100000, '#b2182b',
+                500000, '#b2182b',
+                700000, '#67001f'
             ],
             'fill-opacity': 0.75
         }
