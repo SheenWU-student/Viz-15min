3a8088237c296f494fd729541f99b00a31fc1454
SheenWU-student <ucfnxwu@ucl.ac.uk>
2024-05-07 00:44:21 +0100

    almost done accessibilty map

--- visual_website/js/map.js
+++ visual_website/js/map.js
@@ -3,14 +3,14 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2hlZW41IiwiYSI6ImNscmhxanVhYzAxd28ybHF2ejV6Z
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v11',
-    center: [-0.28, 51.50], // a coordinates of London
+    center: [0.04, 51.50], // a coordinates of London
     zoom: 9.5
 });
 
 map.on('load', function() {
     map.addSource('ACL', {
         type: 'geojson',
-        data: '../data/ACL_trans.geojson'
+        data: '../data/ACL_dropNA.geojson'
     });
 
     map.addSource('borough', {
@@ -22,7 +22,9 @@ map.on('load', function() {
         id: 'employment_15',
         type: 'fill',
         source: 'ACL',
-        layout: {},
+        layout: {
+            visibility: 'visible' // 设置为可见
+        },
         paint: {
             // 使用 `interpolate` 表达式来为不同的employment_15值设置颜色
             'fill-color': [
@@ -44,6 +46,125 @@ map.on('load', function() {
         }
     });
 
+    map.addLayer({
+        id: 'nearest_main_bua',
+        type: 'fill',
+        source: 'ACL',
+        layout: {
+            visibility: 'none' // 设置为不可见
+        },
+        paint: {
+            // 使用 `interpolate` 表达式来为不同的employment_15值设置颜色
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'nearest_main_bua'],  
+                0, '#542788',             
+                10, '#998ec3',         
+                30, '#d8daeb',          
+                50, '#f7f7f7',
+                70, '#fee0b6',
+                90, '#f1a340',
+                100, '#b35806'
+                
+            ],
+            'fill-opacity': 0.75
+        }
+    });
+
+    map.addLayer({
+        id: 'gp_number_15',
+        type: 'fill',
+        source: 'ACL',
+        layout: {
+            visibility: 'none' // 设置为不可见
+        },
+        paint: {
+            // 使用 `interpolate` 表达式来为不同的employment_15值设置颜色
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'gp_number_15'],  
+                0, '#2b83ba',             
+                5, '#abdda4',         
+                10, '#ffffbf',          
+                15, '#fdae61',
+                20, '#d7191c',
+            ],
+            'fill-opacity': 0.75
+        }
+    });
+
+    map.addLayer({
+        id: 'hospitals_15',
+        type: 'fill',
+        source: 'ACL',
+        layout: {
+            visibility: 'none' // 设置为不可见
+        },
+        paint: {
+            // 使用 `interpolate` 表达式来为不同的employment_15值设置颜色
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'hospitals_15'],  
+                0, '#4dac26',             
+                5, '#b8e186',         
+                10, '#f1b6da',          
+                15, '#d01c8b',
+                
+            ],
+            'fill-opacity': 0.75
+        }
+    });
+
+    map.addLayer({
+        id: 'school_primary_15',
+        type: 'fill',
+        source: 'ACL',
+        layout: {
+            visibility: 'none' // 设置为不可见
+        },
+        paint: {
+            // 使用 `interpolate` 表达式来为不同的employment_15值设置颜色
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'school_primary_15'],  
+                0, '#01665e',
+                5, '#5ab4ac',
+                10, '#c7eae5',
+                15, '#f5f5f5',
+                20, '#f6e8c3',          
+                25, '#d8b365',         
+                30, '#8c510a',             
+                
+            ],
+            'fill-opacity': 0.75
+        }
+    });
+
+    map.addLayer({
+        id: 'school_secondary_15',
+        type: 'fill',
+        source: 'ACL',
+        layout: {},
+        paint: {
+            // 使用 `interpolate` 表达式来为不同的employment_15值设置颜色
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'school_primary_15'],  
+                0, '#d7191c',
+                2, '#fdae61',
+                4, '#ffffbf',
+                6, '#a6d96a',
+                7, '#1a9641',
+            ],
+            'fill-opacity': 0.75
+        }
+    });
+
     map.addLayer({      // add stroke on boroughs
         id: 'borough-stroke',
         type: 'line',
@@ -59,30 +180,110 @@ map.on('load', function() {
         }
     });
 
-    // document.getElementById('toggle-map-icon').addEventListener('click', function() {
-    //     var mapElement = document.getElementById('map');
-    //     if (mapElement.style.display === 'none') {
-    //         mapElement.style.display = 'block'; // 显示地图
-    //         this.className = 'bi bi-eye'; // 更新图标为眼睛打开
-    //     } else {
-    //         mapElement.style.display = 'none'; // 隐藏地图
-    //         this.className = 'bi bi-eye-slash'; // 更新图标为眼睛关闭
-    //     }
-    // });
-
-    document.getElementById('toggle-map-icon').addEventListener('click', function() {
+    document.getElementById('toggle-map-icon-em').addEventListener('click', function() {
         var layerVisibility = map.getLayoutProperty('employment_15', 'visibility');
+        var legendList = this.parentNode.nextElementSibling; 
     
         // 切换图层的可见性
         if (layerVisibility === 'visible') {
             map.setLayoutProperty('employment_15', 'visibility', 'none'); // 隐藏图层
             this.className = 'bi bi-eye-slash'; // 更改图标为眼睛关闭
+            this.style.color = '#8f8f8f'; // 设置图标颜色为浅灰色
+            legendList.style.display = 'none';
         } else {
             map.setLayoutProperty('employment_15', 'visibility', 'visible'); // 显示图层
             this.className = 'bi bi-eye'; // 更改图标为眼睛打开
+            this.style.color = '#333';
+            legendList.style.display = 'block';
+        }
+    });
+
+    document.getElementById('toggle-map-icon-urban').addEventListener('click', function() {
+        var layerVisibility = map.getLayoutProperty('nearest_main_bua', 'visibility');
+        var legendList = this.parentNode.nextElementSibling; 
+    
+        // 切换图层的可见性
+        if (layerVisibility === 'visible') {
+            map.setLayoutProperty('nearest_main_bua', 'visibility', 'none'); // 隐藏图层
+            this.className = 'bi bi-eye-slash'; // 更改图标为眼睛关闭
+            this.style.color = '#8f8f8f'; // 设置图标颜色为浅灰色
+            legendList.style.display = 'none';
+        } else {
+            map.setLayoutProperty('nearest_main_bua', 'visibility', 'visible'); // 显示图层
+            this.className = 'bi bi-eye'; // 更改图标为眼睛打开
+            this.style.color = '#333';
+            legendList.style.display = 'block';
         }
     });
     
+    document.getElementById('toggle-map-icon-gp').addEventListener('click', function() {
+        var layerVisibility = map.getLayoutProperty('gp_number_15', 'visibility');
+        var legendList = this.parentNode.nextElementSibling; 
+
+        if (layerVisibility === 'visible') {
+            map.setLayoutProperty('gp_number_15', 'visibility', 'none'); 
+            this.className = 'bi bi-eye-slash'; 
+            this.style.color = '#8f8f8f'; 
+            legendList.style.display = 'none';
+        } else {
+            map.setLayoutProperty('gp_number_15', 'visibility', 'visible'); 
+            this.className = 'bi bi-eye'; 
+            this.style.color = '#333';
+            legendList.style.display = 'block';
+        }
+    });
+
+    document.getElementById('toggle-map-icon-hosp').addEventListener('click', function() {
+        var layerVisibility = map.getLayoutProperty('hospitals_15', 'visibility');
+        var legendList = this.parentNode.nextElementSibling; // 获取紧跟在layer-title后面的ul元素
+
+        if (layerVisibility === 'visible') {
+            map.setLayoutProperty('hospitals_15', 'visibility', 'none'); 
+            this.className = 'bi bi-eye-slash'; 
+            this.style.color = '#8f8f8f'; 
+            legendList.style.display = 'none'; // 隐藏列表
+        } else {
+            map.setLayoutProperty('hospitals_15', 'visibility', 'visible'); 
+            this.className = 'bi bi-eye'; 
+            this.style.color = '#333';
+            legendList.style.display = 'block'; // 显示列表
+        }
+    });
+
+    document.getElementById('toggle-map-icon-schp').addEventListener('click', function() {
+        var layerVisibility = map.getLayoutProperty('school_primary_15', 'visibility');
+        var legendList = this.parentNode.nextElementSibling; // 获取紧跟在layer-title后面的ul元素
+
+        if (layerVisibility === 'visible') {
+            map.setLayoutProperty('school_primary_15', 'visibility', 'none'); 
+            this.className = 'bi bi-eye-slash'; 
+            this.style.color = '#8f8f8f'; 
+            legendList.style.display = 'none'; // 隐藏列表
+        } else {
+            map.setLayoutProperty('school_primary_15', 'visibility', 'visible'); 
+            this.className = 'bi bi-eye'; 
+            this.style.color = '#333';
+            legendList.style.display = 'block'; // 显示列表
+        }
+    });
+
+    document.getElementById('toggle-map-icon-schs').addEventListener('click', function() {
+        var layerVisibility = map.getLayoutProperty('school_secondary_15', 'visibility');
+        var legendList = this.parentNode.nextElementSibling; // 获取紧跟在layer-title后面的ul元素
+
+        if (layerVisibility === 'visible') {
+            map.setLayoutProperty('school_secondary_15', 'visibility', 'none'); 
+            this.className = 'bi bi-eye-slash'; 
+            this.style.color = '#8f8f8f'; 
+            legendList.style.display = 'none'; // 隐藏列表
+        } else {
+            map.setLayoutProperty('school_secondary_15', 'visibility', 'visible'); 
+            this.className = 'bi bi-eye'; 
+            this.style.color = '#333';
+            legendList.style.display = 'block'; // 显示列表
+        }
+    });
+
 });
 
 
